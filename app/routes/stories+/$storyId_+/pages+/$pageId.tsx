363d8b1eb15c46a3e23ac022cfe15169ac75f89e
zachjustice <github@zachjustice.dev>
2023-06-01 21:06:08 -0400

    page history wip

--- app/routes/stories+/$storyId_+/pages+/$pageId.tsx
+++ app/routes/stories+/$storyId_+/pages+/$pageId.tsx
@@ -2,7 +2,7 @@ import invariant from "tiny-invariant";
 import {type DataFunctionArgs, json} from "@remix-run/node";
 import {prisma} from "~/utils/db.server.ts";
 import {Link, useLoaderData} from "@remix-run/react";
-import {ChoiceEditor} from "~/routes/resources+/choice-editor.tsx";
+import {PageViewer} from "~/routes/resources+/Page.tsx";
 
 export async function loader({params}: DataFunctionArgs) {
     invariant(params.storyId, 'Missing storyId')
@@ -31,28 +31,6 @@ export async function loader({params}: DataFunctionArgs) {
 
 export default function GetPageRoute() {
     const data = useLoaderData<typeof loader>();
-    return (
-        <div>
-            <p className='mb-5'>{data.page.content}</p>
-            <ul key={data.page.id}>
-                {data.page.nextChoices.map(choice => {
-                    let link = choice.nextPageId
-                        ? `/stories/${data.storyId}/pages/${choice.nextPageId}`
-                        : `/stories/${data.storyId}/pages/new?parentChoiceId=${choice.id}`;
-                    return (
-                        <li key={choice.id}>
-                            <Link to={link}>
-                                {choice.content}
-                            </Link>
-                        </li>
-                    )
-                })}
-                {(data.page.nextChoices.length < 4)
-                    && <ChoiceEditor choice={{
-                        parentPageId: data.page.id,
-                        storyId: data.storyId
-                    }}/>}
-            </ul>
-        </div>
-    )
+
+    return <PageViewer page={data.page} storyId={data.storyId}/>
 }
