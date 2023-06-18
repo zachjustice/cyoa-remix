105d10c9d8e8f47a495888ffcd66c32409852210
Zach Justice <zach.j.justice@gmail.com>
2023-06-18 16:27:41 -0400

    adding prettier and pre-commit hooks

--- app/routes/stories+/$storyId+/edit.tsx
+++ app/routes/stories+/$storyId+/edit.tsx
@@ -1,27 +1,35 @@
-import {useFetcher} from "@remix-run/react";
-import {useEffect} from "react";
-import {useLocation} from "react-router";
-import {useOptionalCurrentStory} from "~/context/story-activity-context.tsx";
-import {StoryEditor} from "~/routes/resources+/story-editor.tsx";
-import {loader} from "~/routes/stories+/$storyId+/_index.js";
+import { useFetcher } from '@remix-run/react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router'
+import { useOptionalCurrentStory } from '~/context/story-activity-context.tsx'
+import { StoryEditor } from '~/routes/resources+/story-editor.tsx'
+import { type loader } from '~/routes/stories+/$storyId+/_index.js'
 
 export default function EditStoryRoute() {
-    const location = useLocation();
-    const fetcher = useFetcher<typeof loader>();
-    let currentStory = useOptionalCurrentStory()
-    const path = location.pathname?.replace('/edit', '');
+	const location = useLocation()
+	const fetcher = useFetcher<typeof loader>()
+	let currentStory = useOptionalCurrentStory()
+	const path = location.pathname?.replace('/edit', '')
 
-    useEffect(() => {
-        console.log(`## useEffect fetcher.data=${JSON.stringify(fetcher.data)} currentStory=${JSON.stringify(currentStory)}`)
-        if (!currentStory && fetcher.state === "idle" && fetcher.data == null) {
-            fetcher.load(`${path}?index`)
-        }
-    },  [currentStory, path, fetcher])
+	useEffect(() => {
+		console.log(
+			`## useEffect fetcher.data=${JSON.stringify(
+				fetcher.data,
+			)} currentStory=${JSON.stringify(currentStory)}`,
+		)
+		if (!currentStory && fetcher.state === 'idle' && fetcher.data == null) {
+			fetcher.load(`${path}?index`)
+		}
+	}, [currentStory, path, fetcher])
 
-    console.log(`## edit fetcher.data=${JSON.stringify(fetcher.data)} currentStory=${JSON.stringify(currentStory)}`)
-    if (!currentStory && fetcher.data) {
-        currentStory = fetcher.data?.story
-    }
+	console.log(
+		`## edit fetcher.data=${JSON.stringify(
+			fetcher.data,
+		)} currentStory=${JSON.stringify(currentStory)}`,
+	)
+	if (!currentStory && fetcher.data) {
+		currentStory = fetcher.data?.story
+	}
 
-    return (currentStory && <StoryEditor story={currentStory} />)
+	return currentStory && <StoryEditor story={currentStory} />
 }
