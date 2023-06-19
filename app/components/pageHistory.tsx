562d48397574b553b565227eacae7d41f913f461
Zach Justice <zach.j.justice@gmail.com>
2023-06-18 21:02:08 -0400

    Time travel through page history

--- app/components/pageHistory.tsx
+++ app/components/pageHistory.tsx
@@ -1,29 +1,40 @@
-import {useOptionalCurrentStory, usePageHistory} from "~/context/story-activity-context.tsx";
-import {PageViewer} from "~/routes/resources+/Page.tsx";
+import {
+	useOptionalCurrentStory,
+	usePageHistory,
+} from '~/context/story-activity-context.tsx'
+import { PageViewer } from '~/routes/resources+/Page.tsx'
 
 export function PageHistory() {
-    const currentStory = useOptionalCurrentStory();
-    const pageHistory = usePageHistory();
+	const currentStory = useOptionalCurrentStory()
+	const pageHistory = usePageHistory()
 
-    if (!currentStory) {
-        return null;
-    }
+	if (!currentStory) {
+		return null
+	}
 
-    return <>
-        {pageHistory.reverse().map((page, index) => {
-            return (
-                <div key={page.id} className='pt-6 pb-6 border-t-2 border-t-gray-400 border-b-gray-400 flex flex-row'>
-                    <div className='content-center flex-none w-7'>
-                        <p className='text-gray-400 font-bold'>{pageHistory.length - index}</p>
-                    </div>
-                    <div className='col-span-11'>
-                        <PageViewer editable={false}
-                                    page={page}
-                                    storyId={currentStory.id}
-                        />
-                    </div>
-                </div>
-            )
-        })}
-    </>
+	return (
+		<>
+			{pageHistory.reverse().map((page, index) => {
+				return (
+					<div
+						key={page.id}
+						className="flex flex-row border-t-2 border-b-gray-400 border-t-gray-400 pb-6 pt-6"
+					>
+						<div className="w-7 flex-none content-center">
+							<p className="font-bold text-gray-400">
+								{pageHistory.length - index}
+							</p>
+						</div>
+						<div className="col-span-11 ">
+							<PageViewer
+								editable={false}
+								page={page}
+								storyId={currentStory.id}
+							/>
+						</div>
+					</div>
+				)
+			})}
+		</>
+	)
 }
--- app/context/story-activity-context.tsx
+++ app/context/story-activity-context.tsx
@@ -1,108 +1,138 @@
-import React, {createContext, useContext, useReducer} from 'react';
-import {type ViewedChoice, type ViewedPage} from "~/routes/stories+/$storyId+/pages+/$pageId.tsx";
+import React, { createContext, useContext, useReducer } from 'react'
+import {
+	type ViewedChoice,
+	type ViewedPage,
+} from '~/routes/stories+/$storyId+/pages+/$pageId+/_index.tsx'
 
 export type CurrentStory = {
-    id: string,
-    title: string,
-    firstPageId: string|null,
-    description: string,
-    createdAt: string,
-    updatedAt: string,
-    ownerId: string,
-    owner: {
-        id: string,
-        username: string,
-    }
+	id: string
+	title: string
+	firstPageId: string | null
+	description: string
+	createdAt: string
+	updatedAt: string
+	ownerId: string
+	owner: {
+		id: string
+		username: string
+	}
 }
 
-export function isCurrentStory(currentStory: any): currentStory is CurrentStory {
-    return currentStory && typeof currentStory === 'object' && typeof currentStory.id === 'string'
+export function isCurrentStory(
+	currentStory: any,
+): currentStory is CurrentStory {
+	return (
+		currentStory &&
+		typeof currentStory === 'object' &&
+		typeof currentStory.id === 'string'
+	)
 }
 
 interface StoryActivityChoice extends ViewedChoice {
-    isChosen: Boolean | undefined;
+	isChosen: Boolean | undefined
 }
 
 interface StoryActivityPage extends Omit<ViewedPage, 'nextChoices'> {
-    nextChoices: StoryActivityChoice[]
+	nextChoices: StoryActivityChoice[]
 }
 
 type StoryActivityState = {
-    currentStory: CurrentStory | null,
-    pageHistory: StoryActivityPage[]
+	currentStory: CurrentStory | null
+	pageHistory: StoryActivityPage[]
 }
 
 const initialState: StoryActivityState = {
-    currentStory: null,
-    pageHistory: []
+	currentStory: null,
+	pageHistory: [],
 }
 
-const StoryActivityContext = createContext<StoryActivityState>(initialState);
-const StoryDispatchContext = createContext<React.Dispatch<ActionType>>(() => undefined);
+const StoryActivityContext = createContext<StoryActivityState>(initialState)
+const StoryDispatchContext = createContext<React.Dispatch<ActionType>>(
+	() => undefined,
+)
 
-export function StoryActivityProvider({children}: React.PropsWithChildren) {
-    const [storyActivity, dispatch] = useReducer(storyActivityReducer, initialState);
+export function StoryActivityProvider({ children }: React.PropsWithChildren) {
+	const [storyActivity, dispatch] = useReducer(
+		storyActivityReducer,
+		initialState,
+	)
 
-    return (
-        <StoryActivityContext.Provider value={storyActivity}>
-                <StoryDispatchContext.Provider value={dispatch}>
-                    {children}
-                </StoryDispatchContext.Provider>
-        </StoryActivityContext.Provider>
-    );
+	return (
+		<StoryActivityContext.Provider value={storyActivity}>
+			<StoryDispatchContext.Provider value={dispatch}>
+				{children}
+			</StoryDispatchContext.Provider>
+		</StoryActivityContext.Provider>
+	)
 }
 
-export function useOptionalCurrentStory(): CurrentStory|null {
-    const maybeCurrentStory = useContext(StoryActivityContext).currentStory;
+export function useOptionalCurrentStory(): CurrentStory | null {
+	const maybeCurrentStory = useContext(StoryActivityContext).currentStory
 
-    return maybeCurrentStory
-        ? JSON.parse(JSON.stringify(maybeCurrentStory)) as CurrentStory
-        : null;
+	return maybeCurrentStory
+		? (JSON.parse(JSON.stringify(maybeCurrentStory)) as CurrentStory)
+		: null
 }
 
 export function useCurrentStory() {
-    const maybeStory = useOptionalCurrentStory();
-    if (!maybeStory) {
-        throw Error('No current story set in StoryActivityContext, but story is required by useCurrentStory. If a current story is optional, try useOptionalCurrentStory instead.')
-    }
-    return maybeStory
+	const maybeStory = useOptionalCurrentStory()
+	if (!maybeStory) {
+		throw Error(
+			'No current story set in StoryActivityContext, but story is required by useCurrentStory. If a current story is optional, try useOptionalCurrentStory instead.',
+		)
+	}
+	return maybeStory
 }
 
 export function usePageHistory(): StoryActivityPage[] {
-    return [...useContext(StoryActivityContext).pageHistory];
+	return [...useContext(StoryActivityContext).pageHistory]
 }
 
 export function useStoryActivityDispatch() {
-    return useContext(StoryDispatchContext);
+	return useContext(StoryDispatchContext)
 }
 
 type ActionType =
-    | { type: 'reset-story-activity' , payload: null }
-    | { type: 'begin-story' , payload: CurrentStory }
-    | { type: 'add-to-page-history', payload: StoryActivityPage };
+	| { type: 'reset-story-activity'; payload: null }
+	| { type: 'begin-story'; payload: CurrentStory }
+	| { type: 'add-to-page-history'; payload: StoryActivityPage }
 
-function storyActivityReducer(storyActivity: StoryActivityState, action: ActionType): StoryActivityState {
-    switch (action.type) {
-        case 'reset-story-activity': {
-            return {
-                pageHistory: [],
-                currentStory: null,
-            };
-        }
-        case 'begin-story': {
-            return {
-                pageHistory: [],
-                currentStory: action.payload,
-            };
-        }
-        case 'add-to-page-history': {
-            return {
-                ...storyActivity,
-                pageHistory: storyActivity.pageHistory.concat(action.payload),
-            };
-        }
-        default: {
-            throw Error(`Unknown action in StoryActivityReducer: ${JSON.stringify(action)}`);
-        }
-    }
+function storyActivityReducer(
+	storyActivity: StoryActivityState,
+	action: ActionType,
+): StoryActivityState {
+	switch (action.type) {
+		case 'reset-story-activity': {
+			return {
+				pageHistory: [],
+				currentStory: null,
+			}
+		}
+		case 'begin-story': {
+			return {
+				pageHistory: [],
+				currentStory: action.payload,
+			}
+		}
+		case 'add-to-page-history': {
+			let pastPageIndex = storyActivity.pageHistory.findIndex(
+				page => page.id === action.payload.id,
+			)
+
+			pastPageIndex =
+				pastPageIndex > -1 ? pastPageIndex : storyActivity.pageHistory.length
+
+			return {
+				...storyActivity,
+				pageHistory: storyActivity.pageHistory
+					.slice(0, pastPageIndex)
+					.concat(action.payload),
+			}
+		}
+		default: {
+			throw Error(
+				`Unknown action in StoryActivityReducer: ${JSON.stringify(action)}`,
+			)
+		}
+	}
 }
