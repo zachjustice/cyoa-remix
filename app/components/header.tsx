dec6a11651e205c7d288c603145fad6cd097ab28
zachjustice <github@zachjustice.dev>
2023-11-28 18:19:39 -0500

    Fix sidebar icon

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -25,7 +25,22 @@ const Header = function ({ user }: HeaderProps) {
 							className="mr-2 cursor-pointer rounded p-2 text-2xl text-gray-200 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:ring-2 focus:ring-gray-100 active:text-gray-900  dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:bg-gray-700 dark:focus:ring-gray-700 lg:hidden"
 							onClick={() => setOpenOnSmallScreens(!isOpenOnSmallScreens)}
 						>
-							<FaBook />
+							{!isOpenOnSmallScreens ? (
+								<FaBook />
+							) : (
+								<svg
+									className="h-6 w-6"
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fillRule="evenodd"
+										d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+										clipRule="evenodd"
+									></path>
+								</svg>
+							)}
 						</button>
 					)}
 					<Navbar.Brand href="/">
